0dba4da879930ea70b8a90394e76a788705c723a
Parv3213 <parv3213@gmail.com>
2022-12-20 16:46:09 +0530

    Post new workout

--- backend/routes/workout.ts
+++ backend/routes/workout.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 
+import Workout from '../models/workoutModel'
+
 const router = express.Router()
 
 // GET all workouts
@@ -13,8 +15,18 @@ router.get('/:id', (req, res) => {
 })
 
 // POST a new workout
-router.post('/', (req, res) => {
-  res.json({ msg: 'Post a new workout' })
+router.post('/', async (req, res) => {
+  const { title, load, reps } = req.body
+  try {
+    const workout = await Workout.create({
+      title,
+      load,
+      reps,
+    })
+    res.status(200).json(workout)
+  } catch (error) {
+    res.status(400).json({ error: error })
+  }
 })
 
 // DELETE a new workout
